65bf751e0486e85be0e29e66b8d979a1fb744894
Dien Luong <dysonlu@protonmail.com>
2020-04-03 23:00:39 -0400

    New: added react-router

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+          <Link to="/world">Hello!</Link>
+        </header>
+        <Switch>
+          <Route path="/world">
+            <h2>Hello World</h2>
+            <Link to="/">Goodbye!</Link>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
