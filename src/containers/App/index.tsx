2b75b8cda81e6cbe34e6602eb26ba048ce4e25cf
Dien Luong <dysonlu@protonmail.com>
2020-04-04 02:27:30 -0400

    New: added firebase

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
+};
+
+firebase.initializeApp(firebaseConfig);
 
 function App(): JSX.Element {
   return (
